88d9f82b469e0c1f5f06e9ccb65bab28854558ec
IrishDec <declanobrien77@gmail.com>
2025-09-07 11:31:29 +0100

    Create config.example.js

---
+++ admin/admin/config.example.js
@@ -0,0 +1,5 @@
+// Copy this to admin/config.js and fill in your real values (do NOT commit config.js)
+window.FRJOE_CONFIG = {
+  SUPABASE_URL: "https://bwjtrbxexxzowyjpbxtu.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3anRyYnhleHh6b3d5anBieHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNzEwNzcsImV4cCI6MjA3Mjc0NzA3N30.zeo1fB1FSpwj2ayIIh_KIw4MBLfgSLVkhvJIeS516hc"
+};
